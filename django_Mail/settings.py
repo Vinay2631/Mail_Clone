859644e708f855c786e9939f16de538eec725efa
Vinay2631 <kammarivinay26@gmail.com>
2023-07-12 07:44:59 +0530

    debug set to false

--- django_Mail/settings.py
+++ django_Mail/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yd)+2&493zsp$$s=jqder191(_^=po&wp8dbv8xf)2fe&r$iu)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
