6330a4b40b3e3a7e0d8c126c0d799c6fc52fa56a
Vinay2631 <kammarivinay26@gmail.com>
2023-07-12 07:31:33 +0530

    allo-se,app-wsgi,req,ver

--- django_Mail/settings.py
+++ django_Mail/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yd)+2&493zsp$$s=jqder191(_^=po&wp8dbv8xf)2fe&r$iu)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
